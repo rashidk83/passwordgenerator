a4343f1461ed5c15863a2d0bafa91a1c6cc01357
Henry Kariuki <kariukihenry83@gmail.com>
2021-03-12 20:50:13 -0600

    Added more variables

--- script.js
+++ script.js
@@ -9,6 +9,22 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characterCount;
+
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var arrayLowercase = lowerCase.split("");
+
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var arrayUppercase = uppercase.split("");
+
+var numbers = "0123456789";
+var arrayNumbers = numbers.split("");
+
+var special = "!@#$%^&*";
+var arrayspecial = special.split("");
+
+var allarrays = [];
+
 
 function genaratePrompt () {
   var characters= prompt ("how many characters");
@@ -22,16 +38,22 @@ function genaratePrompt () {
 return;
 }
 
+
+
 function generatePassword (){ 
-var password = "blank text";
-var characters= prompt ("how many characters");
-characterCount = parseInt (characters)
+var password = "";
 
-if(characterCount<8 || characterCount>128 || isNaN(characterCount)) {
- alert ("Please try again")
+var lc = confirm("include lower case");
 
+if (lc === true){
 
 }
+
+var uc = confirm("include uppercase");
+
+var numbers = confirm("include numbers");
+
+var sc = confirm ("include special characters");
 
 
 return password;
